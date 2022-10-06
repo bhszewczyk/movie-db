1c01fb1c25e4ea5d1f7908cac967d94fd5a4421c
Barbara Szewczyk <comamysza@gmail.com>
2022-10-07 01:14:20 +0200

    Add movie cards based on search results

--- script.js
+++ script.js
@@ -9,19 +9,77 @@ const SEARCH_API =
 // get elements from the DOM
 const form = document.getElementById('search-form');
 const search = document.getElementById('search');
+const movieCardsContainer = document.getElementById('main');
 
 // Get initial movies
 getMovies(API_URL);
 
+// Clear movie cards
+function clearMovies() {
+	main.innerHTML = '';
+}
+
+// Display movies
+function showMovies(movies) {
+	clearMovies();
+
+	movies.forEach((movie) => {
+		const {
+			title,
+			vote_average: rating,
+			poster_path: imgPath,
+			overview,
+		} = movie;
+
+		const movieEl = document.createElement('div');
+		movieEl.classList.add('movie');
+
+		const htmlTemplate = `
+				<img
+					src="${IMG_PATH + imgPath}"
+					alt="\"${title}\" poster"
+				/>
+				<div class="movie-info">
+					<h3>${title}</h3>
+					<span class="${getRatingColor(rating)}">${rating}</span>
+				</div>
+				<div class="overview">
+					<h3>Overview</h3>
+					<p>
+						${overview}
+					</p>
+				</div>`;
+
+		movieEl.innerHTML = htmlTemplate;
+
+		main.append(movieEl);
+	});
+}
+
+// Get movies from the database
 function getMovies(url) {
 	fetch(url)
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
+			const movies = data.results;
+			console.log(movies);
+			showMovies(movies);
 		})
 		.catch((error) => console.error('Error:', error));
 }
 
+// Set the rating color
+function getRatingColor(rating) {
+	if (rating >= 7) {
+		return 'green';
+	} else if (rating < 7 && rating > 5) {
+		return 'orange';
+	} else {
+		return 'red';
+	}
+}
+
+// On search fetch movies
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
